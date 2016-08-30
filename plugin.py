ec67eb045ba919ab42e1903e9fe73436268bfae7
Thinkofname <thinkofdeath@spigotmc.org>
2016-08-30 13:39:30 +0100

    Minor cleanup

--- plugin.py
+++ plugin.py
@@ -20,7 +20,6 @@ class rustPluginSyntaxCheckEvent(sublime_plugin.EventListener):
             )
             output = cargoRun.communicate()
             view.erase_phantoms('buildErrorLine')
-            view_filename = view.file_name()
 
             for line in output[1].split('\n'):
                 if line == '' or line[0] != '{':
@@ -29,40 +28,51 @@ class rustPluginSyntaxCheckEvent(sublime_plugin.EventListener):
                 # Can't show without spans
                 if len(info['spans']) == 0:
                     continue
+                self.add_error_phantom(view, info)
 
-                msg = info['message']
-                base_color = "#F00"
-                if info['level'] != "error":
-                    base_color = "#FF0"
+    def add_error_phantom(self, view, info):
+        msg = info['message']
 
-                for span in info['spans']:
-                    if not view_filename.endswith(span['file_name']):
-                        continue
-                    color = base_color
-                    char = "^"
-                    if not span['is_primary']:
-                        color = "#0FF"
-                        char = "-"
-                    area = sublime.Region(
-                        view.text_point(span['line_start'] - 1, span['column_start'] - 1),
-                        view.text_point(span['line_end'] - 1, span['column_end'] - 1)
-                    )
-                    underline = char * (span['column_end'] - span['column_start'])
-                    label = span['label']
-                    if not label:
-                        label = ''
-                    view.add_phantom(
-                        'buildErrorLine', area,
-                        "<span style=\"color:{}\">{} {}</span>"
-                        .format(color, underline, html.escape(label, quote=False)),
-                        sublime.LAYOUT_BELOW
-                    )
-                    if span['is_primary']:
-                        view.add_phantom(
-                            'buildErrorLine', area,
-                            "<span style=\"color:{}\">{}</span>"
-                            .format(color,  html.escape(msg, quote=False)),
-                            sublime.LAYOUT_BELOW
-                        )
+        base_color = "#F00" # Error color
+        if info['level'] != "error":
+            # Warning color
+            base_color = "#FF0"
+
+        view_filename = view.file_name()
+        for span in info['spans']:
+            if not view_filename.endswith(span['file_name']):
+                continue
+            color = base_color
+            char = "^"
+            if not span['is_primary']:
+                # Non-primary spans are normally additional
+                # information to help understand the error.
+                color = "#0FF"
+                char = "-"
+            # Sublime text is 0 based whilst the line/column info from
+            # rust is 1 based.
+            area = sublime.Region(
+                view.text_point(span['line_start'] - 1, span['column_start'] - 1),
+                view.text_point(span['line_end'] - 1, span['column_end'] - 1)
+            )
+
+            underline = char * (span['column_end'] - span['column_start'])
+            label = span['label']
+            if not label:
+                label = ''
+
+            view.add_phantom(
+                'buildErrorLine', area,
+                "<span style=\"color:{}\">{} {}</span>"
+                .format(color, underline, html.escape(label, quote=False)),
+                sublime.LAYOUT_BELOW
+            )
+            if span['is_primary']:
+                view.add_phantom(
+                    'buildErrorLine', area,
+                    "<span style=\"color:{}\">{}</span>"
+                    .format(color,  html.escape(msg, quote=False)),
+                    sublime.LAYOUT_BELOW
+                )
 
 
