85b998627983636b5419eb9a233a063d14740b46
PSeitz <PSeitz@users.noreply.github.com>
2017-03-16 00:11:02 +0100

    show syntax check status in status bar after toggle (#163)
    * show syntax check status in status bar after toggle
    
    * flip
    
    * Update toggle_setting.py

--- toggle_setting.py
+++ toggle_setting.py
@@ -6,3 +6,4 @@ class ToggleRustSyntaxSettingCommand(sublime_plugin.TextCommand):
         # Grab the setting and reserse it
         current_state = self.view.settings().get('rust_syntax_checking')
         self.view.settings().set('rust_syntax_checking', not current_state)
+        self.view.window().status_message("Rust syntax checking is now " + ("inactive" if current_state else "active"))
