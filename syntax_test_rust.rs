2b6a8f0034a6e43275b85c1e6c749ad396011cb7
ehuss <eric@huss.org>
2018-07-09 04:33:23 -0700

    Syntax: More stringent escape handling. (#297)
    This makes a few changes to strings and byte/char literals:
    
    - Allow _ in unicode escapes.
    - Reject byte/char literals with too many chars.
    - Reject non-ascii in byte literals.
    - Reject ascii escape with >0x7f.
    - Reject bad escape sequences.
    
    cc #284

--- syntax_test_rust.rs
+++ syntax_test_rust.rs
@@ -84,6 +84,107 @@ let raw_bytes = br#"This won't escape anything either \x01 \""#;
 //              ^^ storage.type
 //                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ string.quoted.double - constant.character.escape
 
+let b_simple = b'a';
+//             ^^^^ string.quoted.single
+//             ^ storage.type.string
+//              ^ punctuation.definition.string.begin
+//                ^ punctuation.definition.string.end
+//                 ^ punctuation.terminator
+let b_newline = b'\n';
+//              ^^^^^ string.quoted.single
+//                ^^ string.quoted.single constant.character.escape
+let b_nul = b'\0';
+//            ^^ string.quoted.single constant.character.escape
+let b_back = b'\\';
+//             ^^ string.quoted.single constant.character.escape
+let b_quote = b'\'';
+//              ^^ string.quoted.single constant.character.escape
+let b_esc_nul = b'\x00';
+//                ^^^^ string.quoted.single constant.character.escape
+let b_esc_255 = b'\xff';
+//                ^^^^ string.quoted.single constant.character.escape
+let b_esc_inv = b'\a';
+//                ^^ invalid.illegal.byte
+//                  ^ string.quoted.single punctuation.definition.string.end
+let b_inv_len = b'abc';
+//                ^ string.quoted.single
+//                 ^^ invalid.illegal.byte
+//                   ^ string.quoted.single punctuation.definition.string.end
+let b_inv_uni = b'♥';
+//                ^ invalid.illegal.byte
+//                 ^ string.quoted.single punctuation.definition.string.end
+let b_inv_empty = b'';
+//                ^^^ string.quoted.single
+//                 ^ punctuation.definition.string.begin
+//                  ^ punctuation.definition.string.end
+let b_unclosed1 = b'
+// Avoid error on entire file.
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.line.double-slash - invalid - string
+
+let bs_newline = b"abc\n";
+//               ^^^^^^^^ string.quoted.double
+//                ^ punctuation.definition.string.begin
+//                    ^^ constant.character.escape
+//                      ^ punctuation.definition.string.end
+//                       ^ punctuation.terminator
+let bs_nul = b"abc\0";
+//                ^^ string.quoted.double constant.character.escape
+let bs_esc_nul = b"abc\x00";
+//                    ^^^^ string.quoted.double constant.character.escape
+let bs_esc_255 = b"abc\xff";
+//                    ^^^^ string.quoted.double constant.character.escape
+let bs_esc_inv = b"abc\a";
+//                    ^^ string.quoted.double invalid.illegal.character.escape
+//                      ^ string.quoted.double punctuation.definition.string.end - invalid
+
+let char_newline = '\n';
+//                 ^^^^ string.quoted.single
+//                 ^ punctuation.definition.string.begin
+//                  ^^ constant.character.escape
+//                    ^ punctuation.definition.string.end
+//                     ^ punctuation.terminator
+let char_nul = '\0';
+//              ^^ string.quoted.single constant.character.escape
+let char_extra_inv = 'ab';
+//                    ^ string.quoted.single
+//                     ^ invalid.illegal.char
+//                      ^ string.quoted.single punctuation.definition.string.end
+let char_ascii_esc_nul = '\x00';
+//                        ^^^^ string.quoted.single constant.character.escape
+let char_ascii_esc_127 = '\x7f';
+//                        ^^^^ string.quoted.single constant.character.escape
+let char_ascii_inv_255 = '\xff';
+//                        ^^^^ invalid.illegal.char
+let char_uni_esc = '\u{3b1}';
+//                  ^^^^^^^ string.quoted.single constant.character.escape
+let char_uni_esc_empty = '\u{}';
+//                        ^^^^ invalid.illegal.char
+let char_uni_esc_under_start = '\u{_1_}';
+//                              ^^^^^^^ invalid.illegal.char
+let char_uni_esc_under1 = '\u{1_}';
+//                         ^^^^^^ string.quoted.single constant.character.escape
+let char_uni_esc_under2 = '\u{1_2__3___}';
+//                         ^^^^^^^^^^^^^ string.quoted.single constant.character.escape
+let char_uni_esc_under3 = '\u{10__FFFF}';
+//                         ^^^^^^^^^^^^ string.quoted.single constant.character.escape
+let char_uni_esc_extra = '\u{1234567}';
+//                        ^^^^^^^^^^^ invalid.illegal.char
+
+let s_ascii_inv_255 = "\xff";
+//                     ^^ string.quoted.double invalid.illegal.character.escape
+let s_uni_esc_empty = "\u{}";
+//                     ^^^^ string.quoted.double invalid.illegal.character.escape
+let s_uni_esc_under_start = "\u{_1_}";
+//                           ^^^^^^^ string.quoted.double invalid.illegal.character.escape
+let s_uni_esc_under1 = "\u{1_}";
+//                      ^^^^^^ string.quoted.double constant.character.escape
+let s_uni_esc_under2 = "\u{1_2__3___}";
+//                      ^^^^^^^^^^^^^ string.quoted.double constant.character.escape
+let s_uni_esc_under3 = "\u{10__FFFF}";
+//                      ^^^^^^^^^^^^ string.quoted.double constant.character.escape
+let s_uni_esc_extra = "\u{1234567}";
+//                     ^^^^^^^^^^^ string.quoted.double invalid.illegal.character.escape
+
 0;
 // <- constant.numeric.integer.decimal
 1_000u32;
