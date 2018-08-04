b9a965fbb302f177e8723e9a331681b4f191030f
Eric Huss <eric@huss.org>
2018-08-03 19:51:25 -0700

    Syntax: Rewrite macro_rules
    macro_rules failed in a lot of cases, this reworks it to fully support:
    - Complete matcher syntax.
    - Better bracket balancing support.
    - Add `lifetime` designator (1.28).
    - More resilient to odd syntax within the transcriber.  The content doesn't have
      to be Rust syntax, so it can sometimes get confused. Add a few escapes so it
      at least doesn't barf.
    - At-most-once kleene operator (2018).

--- tests/syntax-rust/syntax_test_macros.rs
+++ tests/syntax-rust/syntax_test_macros.rs
@@ -99,8 +99,91 @@ my_var = format!("Hello {name}, how are you?",
 //              ^ punctuation.definition.group.begin
 //               ^ punctuation.definition.group.end
 
+/*******************************************************************/
+// The outer brackets can be any type.
+macro_rules! brackets_curly {
+//                          ^ meta.macro punctuation.section.block.begin
+    ($i:ident) => ($i)
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//             ^^ meta.macro keyword.operator
+//                ^^^^ meta.macro meta.macro.transcribers
+}
+// <- meta.macro punctuation.section.block.end
+macro_rules! brackets_paren (
+//                          ^ meta.macro punctuation.section.block.begin
+    ($i:ident) => ($i)
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//             ^^ meta.macro keyword.operator
+//                ^^^^ meta.macro meta.macro.transcribers
+  );
+//^ meta.macro punctuation.section.block.end
+// ^ punctuation.terminator
+macro_rules! brackets_square [
+//                           ^ meta.macro punctuation.section.block.begin
+    ($i:ident) => ($i)
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//             ^^ meta.macro keyword.operator
+//                ^^^^ meta.macro meta.macro.transcribers
+  ];
+//^ meta.macro punctuation.section.block.end
+// ^ punctuation.terminator
+
+/*******************************************************************/
+// Matchers and transcribers can use any bracket type.
+macro_rules! brackets {
+//^^^^^^^^^^ meta.macro support.function
+//           ^^^^^^^^ meta.macro entity.name.macro
+//                    ^ meta.macro punctuation.section.block.begin
+    ($i:ident) => ($i);
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//                ^^^^ meta.macro meta.macro.transcribers
+//  ^ punctuation.section.block.begin
+//   ^^ variable.parameter
+//     ^ punctuation.separator
+//      ^^^^^ storage.type
+//           ^ punctuation.section.block.end
+//             ^^ meta.macro keyword.operator
+//                ^ punctuation.section.block.begin
+//                 ^^ variable.other
+//                   ^ punctuation.section.block.end
+//                    ^ meta.macro punctuation.terminator
+    {$i:ident} => {$i};
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//                ^^^^ meta.macro meta.macro.transcribers
+//  ^ punctuation.section.block.begin
+//   ^^ variable.parameter
+//      ^^^^^ storage.type
+//           ^ punctuation.section.block.end
+//                ^ punctuation.section.block.begin
+//                   ^ punctuation.section.block.end
+//                    ^ meta.macro punctuation.terminator
+    [$i:ident] => [$i];
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//                ^^^^ meta.macro meta.macro.transcribers
+//  ^ punctuation.section.block.begin
+//   ^^ variable.parameter
+//      ^^^^^ storage.type
+//           ^ punctuation.section.block.end
+//                ^ punctuation.section.block.begin
+//                   ^ punctuation.section.block.end
+    // And they don't have to match.
+    ($i:ident) => [$i];
+//  ^^^^^^^^^^ meta.macro meta.macro.matchers
+//                ^^^^ meta.macro meta.macro.transcribers
+    // Support nested brackets within matcher.
+    ((hello) ($i:ident) [foo] {bar}) => {$i};
+//  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//            ^^ variable.parameter
+//               ^^^^^ storage.type
+//                                   ^^ meta.macro keyword.operator
+//                                      ^^^^ meta.macro meta.macro.transcribers
+//                                       ^^ meta.macro meta.macro.transcribers variable.other
+}
+
+/*******************************************************************/
+// Typical example with embedded rust code.
 macro_rules! forward_ref_binop [
-//                             ^ meta.macro meta.group punctuation.definition.group.begin
+//                             ^ meta.macro punctuation.section.block.begin
     (impl $imp:ident, $method:ident for $t:ty, $u:ty) => {
 //        ^^^^ variable.parameter
 //             ^^^^^ storage.type
@@ -111,7 +194,7 @@ macro_rules! forward_ref_binop [
 //                                             ^^ variable.parameter
 //                                                ^^ storage.type
 //                                                    ^^ keyword.operator
-//                                                       ^ meta.macro meta.group meta.block punctuation.definition.block.begin
+//                                                       ^ meta.macro meta.macro.transcribers punctuation.section.block.begin
         impl<'a, 'b> $imp<&'a $u> for &'b $t {
 //      ^^^^ storage.type.impl
 //          ^^^^^^^^ meta.generic
@@ -126,7 +209,7 @@ macro_rules! forward_ref_binop [
 //                                    ^ keyword.operator
 //                                     ^^ storage.modifier.lifetime
 //                                        ^^ variable.other
-//                                           ^ meta.macro meta.group meta.block meta.impl meta.block punctuation.definition.block.begin
+//                                           ^ meta.macro meta.macro.transcribers meta.impl meta.block punctuation.definition.block.begin
             type Output = <$t as $imp<$u>>::Output;
 //                        ^^^^^^^^^^^^^^^^ meta.generic
 //                                        ^^ meta.path
@@ -143,7 +226,7 @@ macro_rules! forward_ref_binop [
 //                                          ^^ punctuation.separator
 //                                             ^^^^^^^^^^^^^^^^ meta.generic
 //                                                             ^^ meta.path
-//                                                                      ^ meta.macro meta.group meta.block meta.impl meta.block meta.block punctuation.definition.block.begin
+//                                                                      ^ meta.macro meta.macro.transcribers meta.impl meta.block meta.block punctuation.definition.block.begin
                 $imp::$method(*self, *other)
 //              ^^^^ variable.other
 //                    ^^^^^^^ variable.other
@@ -155,57 +238,134 @@ macro_rules! forward_ref_binop [
     }
 ]
 
-macro_rules! alternate_group (
-//                           ^ meta.macro meta.group punctuation.definition.group.begin
-    ($a:expr) => (
-//   ^^ variable.parameter
-//      ^^^^ storage.type
-        println!("Test {}!", $a)
-    )
-)
-
+/*******************************************************************/
+// Kleene operators.
 macro_rules! kleene_star {
     ($($arg:tt)+) => (
-//   ^ meta.macro meta.block meta.group keyword.operator
-//    ^ meta.macro meta.block meta.group punctuation.definition.group.begin
-//     ^^^^ meta.macro meta.block meta.group variable.other
-//         ^^^^^ meta.macro meta.block meta.group
-//              ^ meta.macro meta.block meta.group punctuation.definition.group.end
-//                ^ meta.macro meta.block keyword.operator
+//  ^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//   ^ keyword.operator
+//    ^ punctuation.section.group.begin
+//     ^^^^ variable.parameter
+//         ^ punctuation.separator
+//          ^^ storage.type
+//            ^ punctuation.definition.group.end
+//             ^ keyword.operator
+//              ^ punctuation.section.block.end
+//                ^^ meta.macro keyword.operator
+//                   ^ meta.macro meta.macro.transcribers punctuation.section.block.begin
         println!($($arg));
-    ),
+    );
     ($($arg:tt)*) => (
-//     ^^^^ meta.macro meta.block meta.group variable.other
-//         ^^^^^ meta.macro meta.block meta.group
-//              ^ meta.macro meta.block meta.group punctuation.definition.group.end
-//                ^ meta.macro meta.block keyword.operator
+//  ^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//     ^^^^ variable.parameter
+//             ^ keyword.operator
+//              ^ punctuation.section.block.end
+//                ^^ meta.macro keyword.operator
         println!($($arg)*);
-    ),
+    );
     ($($arg:tt);+) => (
-//     ^^^^ meta.macro meta.block meta.group variable.other
-//         ^^^^^^ meta.macro meta.block meta.group
-//               ^ meta.macro meta.block meta.group punctuation.definition.group.end
-//                 ^ meta.macro meta.block keyword.operator
+//  ^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//     ^^^^ variable.parameter
+//          ^^ storage.type
+//             ^ meta.macro meta.macro.matchers
+//              ^ keyword.operator
+//               ^ punctuation.section.block.end
+//                 ^^ meta.macro keyword.operator
         println!($($arg));
-    ),
+    );
     ($($arg:tt),*) => (
-//     ^^^^ meta.macro meta.block meta.group variable.other
-//         ^^^^^^ meta.macro meta.block meta.group
-//               ^ meta.macro meta.block meta.group punctuation.definition.group.end
-//                 ^ meta.macro meta.block keyword.operator
+//  ^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//     ^^^^  variable.parameter
+//          ^^  storage.type
+//             ^
+//              ^  keyword.operator
+//               ^ meta.macro meta.macro.matchers punctuation.section.block.end
+//                 ^^ meta.macro keyword.operator
         println!($($arg)*);
-    )
-}
+    );
+    // Spacing should be ignored.
+    ( $ ( $ arg : tt ) , * ) => ();
+//  ^^^^^^^^^^^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//  ^ punctuation.section.block.begin
+//    ^ keyword.operator
+//      ^ punctuation.section.group.begin
+//        ^^^^^ variable.parameter
+//              ^ punctuation.separator
+//                ^^ storage.type
+//                   ^ punctuation.definition.group.end
+//                       ^ keyword.operator
+//                         ^ punctuation.section.block.end
+//                           ^^ meta.macro keyword.operator
+    // Separators can be any token.
+    ($($foo:tt) else *) => ();
+//  ^^^^^^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//                   ^ keyword.operator
+//                      ^^ meta.macro keyword.operator
+//                         ^^ meta.macro meta.macro.transcribers
 
-macro_rules! eat_numbers {
-    ($count:expr, /*$comment:ident,*/  $msg:expr) => {{
-    //            ^^^^^^^^^^^^^^^^^^^ meta.macro meta.block meta.group comment.block
-    //                                               ^ meta.macro meta.block meta.block punctuation.definition.block.begin
-    //                                                ^ meta.macro meta.block meta.block meta.block punctuation.definition.block.begin
-        let parse_err = concat!("Err parsing value in ", $msg);
-        try!{ eat_numbers(&mut lines, $count, parse_err, missing_err, too_many) }
-    //  ^^^^ support.macro
-    //      ^ meta.macro meta.block meta.block meta.block meta.block punctuation.definition.block.begin
-    }}
-};
- // <- -meta.macro
+    // At-most-once is new in 2018.
+    // Note: This is in flux, but looks like they've landed on a final form.
+    // https://github.com/rust-lang/rust/issues/51934
+    ($($a:ident)? ; $num:expr) => {};
+//  ^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//              ^ keyword.operator
+//                  ^^^^ variable.parameter
+//                                ^^ meta.macro meta.macro.transcribers
+
+}
+// <- meta.macro punctuation.section.block.end
+
+/*******************************************************************/
+// Different matcher types.
+macro_rules! designators {
+    ($i:item,
+//   ^^ variable.parameter
+//      ^^^^ storage.type
+     $b:block,
+//   ^^ variable.parameter
+//      ^^^^^ storage.type
+     $s:stmt,
+//   ^^ variable.parameter
+//      ^^^^ storage.type
+     $p:pat,
+//   ^^ variable.parameter
+//      ^^^ storage.type
+     $e:expr,
+//   ^^ variable.parameter
+//      ^^^^ storage.type
+     $t:ty,
+//   ^^ variable.parameter
+//      ^^ storage.type
+     $i:ident,
+//   ^^ variable.parameter
+//      ^^^^^ storage.type
+     $p:path,
+//   ^^ variable.parameter
+//      ^^^^ storage.type
+     $t:tt,
+//   ^^ variable.parameter
+//      ^^ storage.type
+     $m:meta,
+//   ^^ variable.parameter
+//      ^^^^ storage.type
+     $l:lifetime) => ();
+//   ^^ variable.parameter
+//      ^^^^^^^^ storage.type
+    // And various tokens
+    ("Any token" /*comment*/ true => 3.14 'life 'c' @ struct self) => ();
+//  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.macro meta.macro.matchers
+//   ^^^^^^^^^^^ string.quoted.double
+//               ^^^^^^^^^^^ comment.block
+//                           ^^^^ constant.language
+//                                ^^ keyword.operator
+//                                   ^^^^ constant.numeric.float
+//                                        ^^^^^ storage.modifier.lifetime
+//                                              ^^^ string.quoted.single
+//                                                  ^ keyword.operator
+//                                                    ^^^^^^ storage.type.struct
+//                                                           ^^^^ keyword.other
+//                                                               ^ punctuation.section.block.end
+//                                                                 ^^ meta.macro keyword.operator
+//                                                                    ^^ meta.macro meta.macro.transcribers
+}
+//<- meta.macro punctuation.section.block.end
