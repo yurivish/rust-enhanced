3841af8071a45d0e1848de7a2a6ff74f04a3196d
Eric Huss <eric@huss.org>
2019-02-05 12:07:50 -0800

    Tests: Update minor wording change 1.34

--- tests/error-tests/tests/test_unicode.rs
+++ tests/error-tests/tests/test_unicode.rs
@@ -6,6 +6,6 @@ fn main() {
 //      ^^^NOTE(>=1.17.0) #[warn(unused_variables)]
 //      ^^^NOTE(>=1.21.0,<1.22.0) to disable this warning
 //      ^^^NOTE(>=1.22.0,<1.25.0-beta) to avoid this warning
-//      ^^^HELP(>=1.25.0-beta) consider using `_foo` instead
+//      ^^^HELP(>=1.25.0-beta) consider
 //      ^^^HELP(>=1.25.0-beta) /Accept Replacement:.*_foo/
 }
