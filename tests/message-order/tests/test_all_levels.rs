3738d43cbfb5c8a37d64b524420c8979f3b65295
Eric Huss <eric@huss.org>
2019-06-01 10:02:25 -0700

    Update for 1.37

--- tests/message-order/tests/test_all_levels.rs
+++ tests/message-order/tests/test_all_levels.rs
@@ -6,5 +6,5 @@ fn f2</*WARN 2,1 "tests/test_all_levels.rs:4" " --> tests/test_all_levels.rs:4:8
 pub fn f() {
     let x = Box::new(0u32);
     // This is an error with a note and help.
-    let y: &Trait = /*ERR 1,2 "tests/test_all_levels.rs:9" " --> tests/test_all_levels.rs:9:98"*/x;
+    let y: &dyn Trait = /*ERR 1,2 "tests/test_all_levels.rs:9" " --> tests/test_all_levels.rs:9:103"*/x;
 }
