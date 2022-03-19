8400575e4d9338595c01a3339e15b4cf0ed5c646
Eric Huss <eric@huss.org>
2022-03-19 10:26:12 -0700

    Update tests for 1.61

--- tests/error-tests/examples/no_main.rs
+++ tests/error-tests/examples/no_main.rs
@@ -1,12 +1,18 @@
 // Should display error about no main.
 
   mod no_main_mod;
-//^^^^^^^^^^^^^^^^ERR(>=1.39.0-beta,rust_syntax_checking_include_tests=False) `main` function not found
-//^^^^^^^^^^^^^^^^ERR(>=1.39.0-beta,rust_syntax_checking_include_tests=False) the main function must be defined
-//^^^^^^^^^^^^^^^^NOTE(>=1.39.0-beta,rust_syntax_checking_include_tests=False) you have one or more functions named `main`
+//^^^^^^^^^^^^^^^^ERR(>=1.39.0-beta,<1.61.0-beta,rust_syntax_checking_include_tests=False) `main` function not found
+//^^^^^^^^^^^^^^^^ERR(>=1.39.0-beta,<1.61.0-beta,rust_syntax_checking_include_tests=False) the main function must be defined
+//^^^^^^^^^^^^^^^^NOTE(>=1.39.0-beta,<1.61.0-beta,rust_syntax_checking_include_tests=False) you have one or more functions named `main`
 //^^^^^^^^^^^^^^^^HELP(>=1.39.0-beta,<1.53.0-beta,rust_syntax_checking_include_tests=False) either move the `main`
-//^^^^^^^^^^^^^^^^HELP(>=1.53.0-beta,rust_syntax_checking_include_tests=False) consider moving the `main`
-//^^^^^^^^^^^^^^^^MSG(>=1.39.0-beta,rust_syntax_checking_include_tests=False) See Also: no_main_mod.rs:6
+//^^^^^^^^^^^^^^^^HELP(>=1.53.0-beta,<1.61.0-beta,rust_syntax_checking_include_tests=False) consider moving the `main`
+//^^^^^^^^^^^^^^^^MSG(>=1.39.0-beta,<1.61.0-beta,rust_syntax_checking_include_tests=False) See Also: no_main_mod.rs:6
+//                |ERR(>=1.61.0-beta,rust_syntax_checking_include_tests=False) `main` function not found
+//                |ERR(>=1.61.0-beta,rust_syntax_checking_include_tests=False) the main function must be defined
+//                |NOTE(>=1.61.0-beta,rust_syntax_checking_include_tests=False) you have one or more functions named `main`
+//                |HELP(>=1.61.0-beta,rust_syntax_checking_include_tests=False) consider moving the `main`
+//                |MSG(>=1.61.0-beta,rust_syntax_checking_include_tests=False) See Also: no_main_mod.rs:6
+
 // When --profile=test is used with `cargo check`, this error will not happen
 // due to the synthesized main created by the test harness.
 // end-msg: ERR(<1.39.0-beta,rust_syntax_checking_include_tests=False OR <1.23.0,rust_syntax_checking_include_tests=True) /`?main`? function not found/
--- tests/error-tests/tests/E0005.rs
+++ tests/error-tests/tests/E0005.rs
@@ -16,11 +16,18 @@ fn main() {
 //      ^^^^^^^ERR refutable pattern in local binding
 //      ^^^^^^^MSG(>=1.39.0-beta,<1.44.0-beta) See Also: ↑:1
 //      ^^^^^^^MSG(>=1.44.0-beta) See Also (external): option.rs:
+//      ^^^^^^^MSG(>=1.61.0-beta) See Also (external): option.rs:
 //      ^^^^^^^NOTE(>=1.40.0-beta) `let` bindings require
 //      ^^^^^^^NOTE(>=1.40.0-beta) for more information
 //      ^^^^^^^NOTE(>=1.44.0-beta) the matched value
-//  ^^^^^^^^^^^^^^^^HELP(>=1.40.0-beta) you might want to use `if let`
-//  ^^^^^^^^^^^^^^^^HELP(>=1.40.0-beta) /Accept Replacement:.*/
+//  ^^^^^^^^^^^^^^^^HELP(>=1.40.0-beta,<1.61.0-beta) you might want to use `if let`
+//  ^^^^^^^^^^^^^^^^HELP(>=1.40.0-beta,<1.61.0-beta) /Accept Replacement:.*/
+//  |HELP(>=1.61.0-beta) /Accept Replacement:.*/
+//  |HELP(>=1.61.0-beta) /alternatively.*let_else/
+//                 |HELP(>=1.61.0-beta) you might want to use `if let`
+//                 |HELP(>=1.61.0-beta) you might want to use `if let`
+//                 |HELP(>=1.61.0-beta) /Accept Replacement:.*/
+//                 |HELP(>=1.61.0-beta) /Accept Replacement:.*/
 }
 // Bug: https://github.com/rust-lang/rust/issues/64769
 // start-msg: ERR(>=1.39.0-beta,<1.44.0-beta) not covered
